21d445dd7e6aba81e1ce97cc36a1245bffd075dd
AVISHI VERMA <avishi2906@gmail.com>
2026-05-17 11:04:06 +0530

    Fix:data directory if not exist

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,9 +1,14 @@
 import Database from "better-sqlite3";
 import path from "path";
 import { fileURLToPath } from "url";
+import { mkdirSync } from "fs";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const dbPath = path.join(__dirname, "..", "data", "app.db");
+
+// Create data directory if it doesn't exist
+const dbDir = path.dirname(dbPath);
+mkdirSync(dbDir, { recursive: true });
 
 const db = new Database(dbPath);
 db.pragma("foreign_keys = ON");
